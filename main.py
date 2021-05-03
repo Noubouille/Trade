2fd08adfe27c2b621a002f7d91c67743d35d4865
Anthony <secondesu@gmail.com>
2021-05-03 18:34:09 +0200

    Earn a lot of money

--- main.py
+++ main.py
@@ -1,40 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-        # if os.path.isfile(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'values.txt')):
-        #     os.remove(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'values.txt'))
-        # labels = [
-        #     "date",
-        #     "high",
-        #     "low",
-        #     "open",
-        #     "close",
-        #     "volume",
-        # ]
-        # arr_len = len(self._BTC_ETH_array)
-        # for i in range(arr_len):
-        #     try:
-        #         ts = int(self._BTC_ETH_array[i]["date"])
-        #         readable = datetime.datetime.fromtimestamp(ts).isoformat()
-        #         with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "values.txt"), "a") as value_file:
-        #             value_file.write(f"\nIndex {i} - Date: {readable}\n")
-        #     except KeyError:
-        #         with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "values.txt"), "a") as value_file:
-        #             value_file.write(f"\nIndex {i} - Date: N/A\n")
-        #     for label in labels:
-        #         try:
-        #             with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "values.txt"), 'a') as value_file:
-        #                 value_file.write(f"self._BTC_ETH_array[{i}][{label}]: {self._BTC_ETH_array[i][label]}\n")
-        #             # print(f"self._BTC_ETH_array[{i}][{label}]: {self._BTC_ETH_array[i][label]}", file=sys.stderr)
-        #         except KeyError:
-        #             pass
-
 import sys
 import os
 import datetime
 import statistics
 import math
-
 
 class Trading:
     _input_str = ""
@@ -56,7 +27,7 @@ class Trading:
     def get_settings(self):
         for i in range(10):
             self.get_input()
-            print("le input str :" + self._input_str, file=sys.stderr)
+            # print("le input str :" + self._input_str, file=sys.stderr)
             array = self._input_str.split(" ")
             if (len(array) < 3):
                 print("settings pas bon", file=sys.stderr)
@@ -79,6 +50,13 @@ class Trading:
             splited_str = i.split(",")
             if (splited_str[0] == "BTC_ETH"):
                 # print("BTC_ETH !!!", file=sys.stderr)
+                # self._BTC_ETH_array[-1]["date"] = float(splited_str[1])
+                # self._BTC_ETH_array[-1]["high"] = float(splited_str[2])
+                # self._BTC_ETH_array[-1]["low"] = float(splited_str[3])
+                # self._BTC_ETH_array[-1]["open"] = float(splited_str[4])
+                # self._BTC_ETH_array[-1]["close"] = float(splited_str[5])
+                # self._BTC_ETH_array[-1]["volume"] = float(splited_str[6])
+
                 self._BTC_ETH_array.append({
                     'date': float(splited_str[1]),
                     'high': float(splited_str[2]),
@@ -87,10 +65,17 @@ class Trading:
                     'close': float(splited_str[5]),
                     'volume': float(splited_str[6]),
                 })
-                self._BTC_ETH_array.append({})
+                # self._BTC_ETH_array.append({})
 
 
             if (splited_str[0] == "USDT_BTC"):
+
+                # self._USDT_BTC_array[-1]["date"] = float(splited_str[1])
+                # self._USDT_BTC_array[-1]["high"] = float(splited_str[2])
+                # self._USDT_BTC_array[-1]["low"] = float(splited_str[3])
+                # self._USDT_BTC_array[-1]["open"] = float(splited_str[4])
+                # self._USDT_BTC_array[-1]["close"] = float(splited_str[5])
+                # self._USDT_BTC_array[-1]["volume"] = float(splited_str[6])
                 self._USDT_BTC_array.append({
                     'date': float(splited_str[1]),
                     'high': float(splited_str[2]),
@@ -99,9 +84,16 @@ class Trading:
                     'close': float(splited_str[5]),
                     'volume': float(splited_str[6]),
                 })
-                self._USDT_BTC_array.append({})
+                # self._USDT_BTC_array.append({})
 
             if (splited_str[0] == "USDT_ETH"):
+
+                # self._USDT_ETH_array[-1]["date"] = float(splited_str[1])
+                # self._USDT_ETH_array[-1]["high"] = float(splited_str[2])
+                # self._USDT_ETH_array[-1]["low"] = float(splited_str[3])
+                # self._USDT_ETH_array[-1]["open"] = float(splited_str[4])
+                # self._USDT_ETH_array[-1]["close"] = float(splited_str[5])
+                # self._USDT_ETH_array[-1]["volume"] = float(splited_str[6])
                 self._USDT_ETH_array.append({
                     'date': float(splited_str[1]),
                     'high': float(splited_str[2]),
@@ -110,15 +102,15 @@ class Trading:
                     'close': float(splited_str[5]),
                     'volume': float(splited_str[6]),
                 })
-                self._USDT_ETH_array.append({})
+                # self._USDT_ETH_array.append({})
 
     def buy_crypto(self, currency, amount, stack):
-        # print("SEND: buy USDT_BTC {0}".format(self._current_stack), file=sys.stderr)
         if stack > amount:
-            print(f'buy {currency} {amount}')
+            print(f'buy {currency} {amount}', end='')
 
-    def sell_crypto(self, currency, amount):
-        print("sell USDT_BTC 0.001", end='')
+    def sell_crypto(self, currency, amount, stack):
+        if stack > amount:
+            print(f"sell {currency} {amount}", end='')
 
     # def find_middleBB(self, array, period = 0):
     #     tmp_list = array[:period]
@@ -153,41 +145,51 @@ class Trading:
                 tmp_list.append(x.get(papa))
         return tmp_list
 
-    def buy_or_sell(self, array, currency_string, currency_stack):
+    def buy_or_sell(self, array, currency_string, currency_stack, order_bool):
         period = self._candles_given
-        middleBB = self.find_middleBB(array, period)
+        openArr = list()
+        closeArr = list()
 
+        for x in array[:-1]:
+            if x.get('close') is not None:
+                closeArr.append(x.get('close'))
+            if x.get('open') is not None:
+                openArr.append(x.get('open'))
+
+        middleBB = self.find_middleBB(array, period)
+        # middleBB2 = getmiddleBB(closeArr, period)
+
+        # print("middleBB  =  " + str(middleBB), file=sys.stderr)
+        # print("middleBB2  =  " + str(middleBB2), file=sys.stderr)
         close_list = self.close_open_list(array, period, "close")
         open_list = self.close_open_list(array, period, "open")
 
-        # open_list = []
-
-        # for x in array[:period]:
-        #     if x.get('open') is not None:
-        #         open_list.append(x.get('open'))
 
         get_StD = self.getStandardDeviation(close_list, period)
-        # upperBB = (get_StD * 2)
-        # lowerBB = (get_StD / 2)
 
-        upper = middleBB + (get_StD * 2)
-        lower = middleBB - (get_StD * 2)
+        upperBB = middleBB + (get_StD * 2)
+        lowerBB = middleBB - (get_StD * 2)
 
-        try:
+        # buyValue = 
+        print("lower bb value : " + str(lowerBB), file=sys.stderr)
+        print("currency_stack : " + str(currency_stack), file=sys.stderr)
 
-            print("lower = " + str(lower), file=sys.stderr)
-            print("self.array[-1][close] = " + str(close_list[-1]["close"]), file=sys.stderr)
-            if (close_list[-1] < lower):
-                self.buy_crypto(currency_string, 0.001, currency_stack)
-            elif (close_list[-1] > upper):
-                print("sell " + currency_string + " " + "0.001", end='')
+        if (closeArr[-1] < lowerBB):
+            if (order_bool == True):
+                print(";", end='')
+            # print("I BUY LA STDX ", file=sys.stderr)
+            # print("buy " + str(currency_string) + " " + "0.08", end='')
+            self.buy_crypto(currency_string, 0.08, currency_stack)
+            return True
+        # elif (close_list[-1] > upperBB):
+        #     if (order_bool == True):
+        #         print(";", end='')
+        #     self.sell_crypto(currency_string, 0.001, currency_stack)
+        #     # print("sell " + str(currency_string) + " " + "0.005", end='')
+        #     return True
 
-            else:
-                print("pass")
-                print("je pass", file=sys.stderr)
-
-        except KeyError:
-            pass
+        else:
+            return False
 
 
 
@@ -203,7 +205,7 @@ class Trading:
                 self._USDT_stack = float(splited_str[1])
             else:
                 exit(84)
-            print("fucking " + splited_str[0] + " stacks : " + splited_str[1], file=sys.stderr)
+            # print("fucking " + splited_str[0] + " stacks : " + splited_str[1], file=sys.stderr)
 
     def loop(self):
         while True:
@@ -217,9 +219,31 @@ class Trading:
                         self.current_stack(array[3])
 
             elif (array[0] == "action" and array[1] == "order"):
-                self.buy_or_sell(self._USDT_ETH_array, "USDT_ETH", self._USDT_stack)
-                self.buy_or_sell(self._BTC_ETH_array, "BTC_ETH", self._BTC_stack)
-                self.buy_or_sell(self._USDT_BTC_array, "USDT_BTC", self._USDT_stack)
+                order_bool = False
+                statement_pass = False
+                # statement_pass = order_bool or statement_pass
+                order_bool = self.buy_or_sell(self._USDT_ETH_array, "USDT_ETH", self._USDT_stack, statement_pass)
+                if (order_bool or statement_pass):
+                    statement_pass = True
+                else:
+                    statement_pass = False
+
+                order_bool = self.buy_or_sell(self._BTC_ETH_array, "BTC_ETH", self._BTC_stack, statement_pass)
+                if (order_bool or statement_pass):
+                    statement_pass = True
+                else:
+                    statement_pass = False
+
+                order_bool = self.buy_or_sell(self._USDT_BTC_array, "USDT_BTC", self._USDT_stack, statement_pass)
+                if (order_bool or statement_pass):
+                    statement_pass = True
+                else:
+                    statement_pass = False
+
+                if (statement_pass == False):
+                    print("pass")
+                else:
+                    print("")
             else :
                 print("caca pas bon", file=sys.stderr)
 
